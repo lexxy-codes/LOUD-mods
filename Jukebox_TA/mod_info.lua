d24a4d08c5a84b36c7d64f9c458c62fda42c8e16
lexxy-codes <alex@ali.uk.com>
2023-12-23 17:48:51 +0000

    Total Annihilation

---
+++ Jukebox_TA/mod_info.lua
@@ -0,0 +1,9 @@
+name = "Jukebox - Total Annihilation"
+version = 5
+description = "Adds the Total Annihilation soundtrack to the in-game jukebox."
+author = "Armoured Fish, PhilipJFry, Lexxy"
+url = "http://forums.gaspowered.com/viewtopic.php?t=21520"
+uid = "35b19110-dcd0-11dc-95ff-0800200c9a70"
+icon = "/mods/Jukebox_TA/mod_icon.png"
+exclusive = false
+ui_only = true
