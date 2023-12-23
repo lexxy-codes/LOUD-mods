9fc36043c2c340d2da9b4fb3486b3a43ea110d82
lexxy-codes <alex@ali.uk.com>
2023-12-23 17:48:39 +0000

    Supreme Commander

---
+++ Jukebox_SC/mod_info.lua
@@ -0,0 +1,8 @@
+name = "Jukebox - Supreme Commander"
+uid = "3f09491e-8236-4258-bc8a-895788b0aadc"
+version = 2
+description = "Adds the Supreme Commander soundtrack to the in-game jukebox."
+author = "NeoQuixotic, Lexxy"
+icon = "/mods/Jukebox_SC/mod_icon.png"
+exclusive = false
+ui_only = true
