cb5e4b30eae84cf98d50ea63e783aa45fd56273c
lexxy-codes <alex@ali.uk.com>
2023-12-23 17:48:28 +0000

    Forged Alliance

---
+++ Jukebox_FA/mod_info.lua
@@ -0,0 +1,8 @@
+name = "Jukebox - Forged Alliance"
+version = 1
+description = "Adds the Forged Alliance soundtrack to the in-game jukebox."
+author = "Lexxy"
+uid = "0b41f8d9-696f-4ea5-a13d-79aee8ca7182"
+icon = "/mods/Jukebox_FA/mod_icon.png"
+exclusive = false
+ui_only = true
